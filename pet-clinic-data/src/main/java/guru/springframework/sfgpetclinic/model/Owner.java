7a7aab70ab9e9823b5cac3d99fb25b7b8e873854
baykoc@gmail.com <baykoc@gmail.com>
2020-05-08 00:01:15 +0300

    testasd

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -17,17 +17,17 @@ import java.util.Set;
 public class Owner extends Person {
 
     @Builder
-    public Owner(Long id, String firstName, String lastName, String adress,
+    public Owner(Long id, String firstName, String lastName, String address,
                  String city, String telephone, Set<Pet> pets) {
         super(id, firstName, lastName);
-        this.adress = adress;
+        this.address = address;
         this.city = city;
         this.telephone = telephone;
         this.pets = pets;
     }
 
     @Column(name="address")
-    private String adress;
+    private String address;
     @Column(name="city")
     private String city;
     @Column(name="telephone")
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -21,4 +21,8 @@ public class PetType extends BaseEntitiy {
     private String name;
 
 
+    @Override
+    public String toString() {
+        return name;
+    }
 }
